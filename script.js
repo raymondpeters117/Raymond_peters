2e6c445e1788e8e963d1f2733bec185517411158
RAYMOND PETERS <raymondpeters117@gmail.com>
2026-07-03 06:59:24 -0700

    Add typing effect for words in script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+// Typing effect
+const words = ["Frontend Developer", "Web Designer", "Programmer"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+
+  if (isDeleting) {
+    j--;
+  } else {
+    j++;
+  }
+
+  document.getElementById("typed").textContent = currentWord.substring(0, j);
+
+  if (!isDeleting && j === currentWord.length) {
+    isDeleting = true;
+    setTimeout(type, 1000);
+    return;
+  }
+
+  if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % words.length;
+  }
+
+  setTimeout(type, 100);
+}
+
+type();
