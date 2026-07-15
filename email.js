322635ef425cc1dc293332b929201f45d283d457
RAYMOND PETERS <raymondpeters117@gmail.com>
2026-07-15 19:01:48 +0300

    Update email.js

--- email.js
+++ email.js
@@ -2,46 +2,73 @@
     EMAIL JS
 ==============================*/
 
-// Replace these with your EmailJS values
-const PUBLIC_KEY = "YOUR_PUBLIC_KEY";
-const SERVICE_ID = "YOUR_SERVICE_ID";
-const TEMPLATE_ID = "YOUR_TEMPLATE_ID";
+// Your EmailJS values
+const PUBLIC_KEY = "yBBhs7VyLJJ-0Xhk8";
+const SERVICE_ID = "service_wmjtgwl";
+const TEMPLATE_ID = "template_upuac99";
 
-emailjs.init(PUBLIC_KEY);
 
-const form = document.getElementById("contact-form");
+// Initialize EmailJS
+emailjs.init({
+    publicKey: PUBLIC_KEY
+});
+
+
+const form = document.getElementById("contactForm");
 const status = document.getElementById("status");
 
-form.addEventListener("submit", function (e) {
 
-    e.preventDefault();
 
-    status.innerHTML = "Sending message...";
+if (form) {
 
-    emailjs.sendForm(
-        SERVICE_ID,
-        TEMPLATE_ID,
-        form
-    )
+    form.addEventListener("submit", function (e) {
 
-    .then(function () {
+        e.preventDefault();
 
-        status.innerHTML = "✅ Message sent successfully!";
 
-        status.style.color = "#00ff88";
+        status.innerHTML = "Sending message...";
+        status.style.color = "orange";
 
-        form.reset();
 
-    })
+        emailjs.sendForm(
+            SERVICE_ID,
+            TEMPLATE_ID,
+            form
+        )
 
-    .catch(function (error) {
 
-        console.log(error);
+        .then(function () {
 
-        status.innerHTML = "❌ Failed to send message.";
 
-        status.style.color = "red";
+            status.innerHTML =
+            "✅ Message sent successfully!";
+
+
+            status.style.color = "#00ff88";
+
+
+            form.reset();
+
+
+        })
+
+
+        .catch(function (error) {
+
+
+            console.log("EmailJS Error:", error);
+
+
+            status.innerHTML =
+            "❌ Failed to send message.";
+
+
+            status.style.color = "red";
+
+
+        });
+
 
     });
 
-});
+}
